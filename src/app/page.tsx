9bdba7761aa28b284aaf37c158560fdd4629cebe
LizaKlev <elizabeth.klevakina@gmail.com>
2025-10-09 11:16:07 +0300

    Database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,44 +2,10 @@ import { DataTable } from '../lib/components/payments/data-table'
 import { columns, Payment } from '@/lib/components/payments/columns'
 // import { Payment } from '@/lib/components/sampleData'
 
-async function getData(): Promise<Payment[]> {
-  // Fetch data from your API here.
-  return [
-    {
-      id: '728ed52f',
-      amount: 100,
-      status: 'pending',
-      email: 'm@example.com',
-    },
-    {
-      id: 'cdbyu493',
-      amount: 300,
-      status: 'success',
-      email: 'trololo.com',
-    },
-    {
-      id: 'kjhgfr56',
-      amount: 900,
-      status: 'success',
-      email: 'tro.com',
-    },
-    {
-      id: 'fr567u',
-      amount: 590,
-      status: 'success',
-      email: 'tlo.com',
-    },
-    {
-      id: ',639dbf',
-      amount: 400,
-      status: 'success',
-      email: 'olo.com',
-    },
-  ]
-}
+import paymentsData from '@/lib/components/payments/payments.json'
 
 export default async function DemoPage() {
-  const data = await getData()
+  const data = paymentsData as Payment[]
 
   return (
     <div className='container flex flex-col mx-auto py-10'>
