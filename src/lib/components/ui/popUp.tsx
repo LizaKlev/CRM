bb9777da2e0c7d6b591ed13a7f3966fdc920f1e5
LizaKlev <elizabeth.klevakina@gmail.com>
2025-10-18 13:43:02 +0300

    testings

--- src/lib/components/ui/popUp.tsx
+++ src/lib/components/ui/popUp.tsx
@@ -1,32 +1,48 @@
 'use client'
 
 import * as React from 'react'
-import { CalendarDays } from 'lucide-react'
+import * as PopoverPrimitive from '@radix-ui/react-popover@1.1.6'
 
-import { Button } from '../ui/button'
-import { Calendar } from '@/lib/components/ui/calendar'
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@/lib/components/ui/popover'
+import { cn } from './utils'
 
-export function DatePickerDemo() {
-  const [date, setDate] = React.useState<Date>()
+function Popover({
+  ...props
+}: React.ComponentProps<typeof PopoverPrimitive.Root>) {
+  return <PopoverPrimitive.Root data-slot='popover' {...props} />
+}
 
+function PopoverTrigger({
+  ...props
+}: React.ComponentProps<typeof PopoverPrimitive.Trigger>) {
+  return <PopoverPrimitive.Trigger data-slot='popover-trigger' {...props} />
+}
+
+function PopoverContent({
+  className,
+  align = 'center',
+  sideOffset = 4,
+  ...props
+}: React.ComponentProps<typeof PopoverPrimitive.Content>) {
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button className='sm:bg-card bg-primary border border-border size-9'>
-          {' '}
-          <CalendarDays className='h-1 w-1 sm:text-white' />{' '}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className='w-auto p-0'>
-        <Calendar mode='single' selected={date} onSelect={setDate} />
-      </PopoverContent>
-    </Popover>
+    <PopoverPrimitive.Portal>
+      <PopoverPrimitive.Content
+        data-slot='popover-content'
+        align={align}
+        sideOffset={sideOffset}
+        className={cn(
+          'bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-72 origin-(--radix-popover-content-transform-origin) rounded-md border p-4 shadow-md outline-hidden',
+          className
+        )}
+        {...props}
+      />
+    </PopoverPrimitive.Portal>
   )
 }
 
-export default DatePickerDemo
+function PopoverAnchor({
+  ...props
+}: React.ComponentProps<typeof PopoverPrimitive.Anchor>) {
+  return <PopoverPrimitive.Anchor data-slot='popover-anchor' {...props} />
+}
+
+export { Popover, PopoverTrigger, PopoverContent, PopoverAnchor }
